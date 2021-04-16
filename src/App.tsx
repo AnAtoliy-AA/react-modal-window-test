9a736bf6ebceecfdbec9831b3fe99b73da051c6e
AnAtoliyAK <aliaksandrau89@gmail.com>
2021-04-16 16:14:41 +0300

    feat: add user form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.scss';
+import UserForm from './components/UserForm/UserForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <UserForm />
       </header>
     </div>
   );
---
+++ src/components/UserForm/UserForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './UserForm.scss';
+
+const UserForm: React.FC = () => (
+  <div className="UserForm">
+    UserForm Component
+  </div>
+);
+
+export default UserForm;
